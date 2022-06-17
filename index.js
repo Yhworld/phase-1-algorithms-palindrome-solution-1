f8c8861c441860c4d19be818f5ee84ce2bfef4a1
Yahya <yahyahussein670@gmail.com>
2022-06-17 12:26:21 +0300

    complete

--- index.js
+++ index.js
@@ -13,10 +13,22 @@ function isPalindrome(word) {
 console.log(isPalindrome('racecar'))
 /* 
   Add your pseudocode here
+  1. isPalindrome(word)
+  2. const wordSplit = word.split('');
+  3. const wordReverse = wordSplit.reverse();
+  3. const wordJoin = wordReverse.join('');
+  4. if (word == wordJoin);
+  5. return true;
+  6. else return false
 */
 
 /*
   Add written explanation of your solution here
+  1. declare the isPalindrome function 
+  2. declare a variable which contains the parameter with the method .split() which split the value of the parameter into array like structure
+  3. declare a variable which contains the previous declared variable with a reverse method which reverses the wordSplit variable
+  4. declare a variable which contains the reversed arguements and use the method join to join the arrays since they were split with the split method during our first variable declarations
+  5. use an if statement to compare the actual parameter vs the reversed variable, return true if the reversed word and actual word is true if not return false.
 */
 
 // You can run `node index.js` to view these console logs
